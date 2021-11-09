e2c9cc9e5afd9881cf884b46d8be89fc4b7e8c72
Apurva Narvekar <93322602+apurva-narvekar@users.noreply.github.com>
2021-11-09 23:15:52 +0530

    Delete Giftshop2 directory
    Not needed

--- Giftshop2/src/main/java/com/example/Giftshop/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Giftshop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.Giftshop.entities.Users;
-
-public interface UsersRepository extends MongoRepository<Users,String>{
-
-}
