fc9f51052aba717c39323e26de239cf9f5cfc1fc
Apurva Narvekar <93322602+apurva-narvekar@users.noreply.github.com>
2021-11-10 09:53:21 +0530

    Delete Giftshop directory

--- Giftshop/src/main/java/com/lti/Giftshop/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lti.Giftshop.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.lti.Giftshop.entities.Category;
-
-public interface CategoryRepository extends MongoRepository<Category,Integer> {
-
-}
